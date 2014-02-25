9d818086a8084d24c460cfb53e4a2530062a21e2
Victor Ashik <victor@ashik.ru>
2014-02-25 13:45:29 +0400

    Initial commit: main.c only

---
+++ main.c
@@ -0,0 +1,21 @@
+/* Copyright (C) Hola 2012, 2013
+ *
+ * Welcome to TheCodeIL.com Challenge!
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+int main(int argc, char *argv[])
+{
+    char *s = NULL;
+    str_cpy(&s, "Hola Hola");
+    str_cpy(&s, s+5);
+    str_cat(&s, " World");
+    str_printf(&s, "%s!", s);
+    puts(s); /* result: "Hola World!" */
+    str_free(&s);
+    return 0;
+}
+
