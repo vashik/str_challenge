7e289a4d9d46db11b216ec442ecacb1805eba5b5
Victor Ashik <victor@ashik.ru>
2014-02-28 18:29:15 +0400

    Remove spaces around "trivial" comarision operators

--- str.c
+++ str.c
@@ -11,7 +11,7 @@ typedef struct {
 char* str_alloc(size_t len)
 {
     str_t* newstr = malloc(len + sizeof(str_t) + 1);
-    if (newstr == NULL)
+    if (newstr==NULL)
         return NULL;
     newstr->size = len;
     return &(newstr->str);
@@ -23,7 +23,7 @@ char* str_realloc(char* s, size_t newlen)
     size_t len = str->size;
     str_t* newstr;
     char*  news;
-    if (newlen <= len)
+    if (newlen<=len)
         return s;
     news = str_alloc(newlen);
     if (news == NULL)
@@ -48,17 +48,17 @@ char* str_cpy(char** dst, const char* src)
     char *olddst = *dst;
     char *newdst;
 #ifdef STR_CHECK_NULL_SRC
-    if (src == NULL)
+    if (src==NULL)
         newdst = NULL;
     else 
 #endif
     {
         srcsize = strlen(src);
-        if (olddst == NULL)
+        if (olddst==NULL)
             newdst = str_alloc(srcsize);
         else
             newdst = str_realloc(olddst, srcsize);
-        if (newdst == NULL)
+        if (newdst==NULL)
             return NULL;
         memmove(newdst, src, srcsize + 1);
     }
@@ -72,20 +72,20 @@ char* str_cat(char** dst, const char* src)
     char *olddst = *dst;
     char *newdst;
 #ifdef STR_CHECK_NULL_SRC
-    if (src == NULL)
+    if (src==NULL)
         return olddst;
 #endif
 #ifdef STR_CHECK_NULL_DST
-    if (dst == NULL || olddst == NULL)
+    if (dst==NULL || olddst == NULL)
         return olddst;
 #endif
     dstsize = strlen(olddst);
     srcsize = strlen(src);
     newdstsize = dstsize + srcsize;
-    if (newdstsize == 0)
+    if (newdstsize==0)
         return olddst;
     newdst = str_realloc(olddst, newdstsize);
-    if (newdst == NULL)
+    if (newdst==NULL)
         return NULL;
     memmove(newdst+dstsize, src, srcsize + 1); 
     (*dst) = newdst;
