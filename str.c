0f6879ccafed35c64a2f88a6f795c7fa945a704c
Victor Ashik <victor@ashik.ru>
2014-02-28 18:12:18 +0400

    Replace each tab with 4 spaces

--- str.c
+++ str.c
@@ -4,110 +4,110 @@
 #include "str.h"
 
 typedef struct {
-	size_t size;
-	char  str;
+    size_t size;
+    char  str;
 } str_t;
 
 char* str_alloc(size_t len)
 {
-	str_t* newstr = malloc(len + sizeof(str_t) + 1);
+    str_t* newstr = malloc(len + sizeof(str_t) + 1);
 
-	if (newstr == NULL) {
-		return NULL;
-	}
+    if (newstr == NULL) {
+        return NULL;
+    }
 
-	newstr->size = len;
-	return &(newstr->str);
+    newstr->size = len;
+    return &(newstr->str);
 }
 
 char* str_realloc(char* s, size_t newlen)
 {
-	str_t* str = (str_t*)(s - offsetof(str_t, str));
-	size_t len = str->size;
-	str_t* newstr;
-	char*  news;
+    str_t* str = (str_t*)(s - offsetof(str_t, str));
+    size_t len = str->size;
+    str_t* newstr;
+    char*  news;
 
-	if (newlen <= len) {
-		return s;
-	}
+    if (newlen <= len) {
+        return s;
+    }
 
-	news = str_alloc(newlen);
-	if (news == NULL) {
-		return NULL;
-	}
-	newstr = (str_t*)(s - offsetof(str_t, str));
-	newstr->size = newlen;
+    news = str_alloc(newlen);
+    if (news == NULL) {
+        return NULL;
+    }
+    newstr = (str_t*)(s - offsetof(str_t, str));
+    newstr->size = newlen;
 
-	memmove(news, s, len + 1);
-	
-	return news;
+    memmove(news, s, len + 1);
+    
+    return news;
 }
 
 void str_free(char** strptr)
 {
-	char* s = *strptr;
-	str_t *str = (str_t*)(s - offsetof(str_t, str));
-	free(str);
-	(*strptr) = NULL;
+    char* s = *strptr;
+    str_t *str = (str_t*)(s - offsetof(str_t, str));
+    free(str);
+    (*strptr) = NULL;
 }
 
 char* str_cpy(char** dst, const char* src)
 {
-	size_t dstsize, srcsize, newdstsize;
-	char *olddst = *dst;
-	char *newdst;
+    size_t dstsize, srcsize, newdstsize;
+    char *olddst = *dst;
+    char *newdst;
 #ifdef STR_CHECK_NULL_SRC
-	if (src == NULL) {
-		newdst = NULL;
-	} else 
+    if (src == NULL) {
+        newdst = NULL;
+    } else 
 #endif
-	{
-		srcsize = strlen(src);
-		if (olddst == NULL) {
-			newdst = str_alloc(srcsize);
-		} else {
-			newdst = str_realloc(olddst, srcsize);
-		}
-		if (newdst == NULL) {
-			return NULL;
-		}	
-		
-		memmove(newdst, src, srcsize + 1);
-	}
+    {
+        srcsize = strlen(src);
+        if (olddst == NULL) {
+            newdst = str_alloc(srcsize);
+        } else {
+            newdst = str_realloc(olddst, srcsize);
+        }
+        if (newdst == NULL) {
+            return NULL;
+        }   
+        
+        memmove(newdst, src, srcsize + 1);
+    }
 
-	(*dst) = newdst;
-	return newdst;
+    (*dst) = newdst;
+    return newdst;
 }
 
 char* str_cat(char** dst, const char* src)
 {
-	size_t dstsize, srcsize, newdstsize;
-	char *olddst = *dst;
-	char *newdst;
+    size_t dstsize, srcsize, newdstsize;
+    char *olddst = *dst;
+    char *newdst;
 
 #ifdef STR_CHECK_NULL_SRC
-	if (src == NULL) {
-		return olddst;
-	}
+    if (src == NULL) {
+        return olddst;
+    }
 #endif
 #ifdef STR_CHECK_NULL_DST
-	if (dst == NULL || olddst == NULL) {
-		return olddst;
-	}
+    if (dst == NULL || olddst == NULL) {
+        return olddst;
+    }
 #endif
-	dstsize = strlen(olddst);
-	srcsize = strlen(src);
-	newdstsize = dstsize + srcsize;
-	if (newdstsize == 0) {
-		return olddst;
-	}		
-	newdst = str_realloc(olddst, newdstsize);
-	if (newdst == NULL) {
-		return NULL;
-	}	
-	memmove(newdst+dstsize, src, srcsize + 1); 
-	(*dst) = newdst;
-	return newdst;
+    dstsize = strlen(olddst);
+    srcsize = strlen(src);
+    newdstsize = dstsize + srcsize;
+    if (newdstsize == 0) {
+        return olddst;
+    }       
+    newdst = str_realloc(olddst, newdstsize);
+    if (newdst == NULL) {
+        return NULL;
+    }   
+    memmove(newdst+dstsize, src, srcsize + 1); 
+    (*dst) = newdst;
+    return newdst;
 }
 
 
