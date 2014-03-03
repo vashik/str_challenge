87cbdcb96715aaccdf985e56727616230ed5ad3e
Victor Ashik <victor@ashik.ru>
2014-03-03 21:42:05 +0400

    str_realloc now has char** input, consistent to other calls

--- str.c
+++ str.c
@@ -3,8 +3,8 @@
 #include <stddef.h>
 #include "str.h"
 
-#define STR_ALLOC_INC           32
-#define STR_ROUNDUP(A)          (((A)/STR_ALLOC_INC+1)*STR_ALLOC_INC)
+#define STR_ALLOC_INC  32
+#define STR_ROUNDUP(A) (((A)/STR_ALLOC_INC+1)*STR_ALLOC_INC)
 
 typedef struct {
     size_t size;
@@ -23,22 +23,23 @@ char* str_alloc(size_t len)
     return &(newstr->str);
 }
 
-char* str_realloc(char* s, size_t newlen)
+char* str_realloc(char** s, size_t newlen)
+/* FIXME: does not check s nor *s for NULL */
 {
-    str_t* str = (str_t*)(s - offsetof(str_t, str));
+    str_t* str = (str_t*)(*s - offsetof(str_t, str));
     size_t len = str->size;
     str_t* newstr;
     char*  news;
     if (newlen<=len)
-        return s;
+        return *s;
     news = str_alloc(newlen);
 #ifdef STR_CHECK_NULL_ALLOCS
     if (news == NULL)
         return NULL;
 #endif
-    newstr = (str_t*)(s - offsetof(str_t, str));
+    newstr = (str_t*)(*s - offsetof(str_t, str));
     newstr->size = newlen;
-    memmove(news, s, len + 1);
+    memmove(news, *s, len + 1);
     return news;
 }
 
@@ -52,8 +53,7 @@ void str_free(char** strptr)
 
 char* str_cpy(char** dst, const char* src)
 {
-    size_t dstsize, srcsize, newdstsize;
-    char *olddst = *dst;
+    size_t srcsize;
     char *newdst;
 #ifdef STR_CHECK_NULL_SRC
     if (src==NULL)
@@ -62,10 +62,10 @@ char* str_cpy(char** dst, const char* src)
 #endif
     {
         srcsize = strlen(src);
-        if (olddst==NULL)
+        if (*dst==NULL)
             newdst = str_alloc(srcsize);
         else
-            newdst = str_realloc(olddst, srcsize);
+            newdst = str_realloc(dst, srcsize);
 #ifdef STR_CHECK_NULL_ALLOCS
         if (newdst==NULL)
             return NULL;
@@ -94,12 +94,12 @@ char* str_cat(char** dst, const char* src)
     newdstsize = dstsize + srcsize;
     if (newdstsize==0)
         return olddst;
-    newdst = str_realloc(olddst, newdstsize);
+    newdst = str_realloc(dst, newdstsize);
 #ifdef STR_CHECK_NULL_ALLOCS
     if (newdst==NULL)
         return NULL;
 #endif
-    memmove(newdst+dstsize, src, srcsize + 1); 
+    memmove(newdst+dstsize, src, srcsize+1); 
     (*dst) = newdst;
     return newdst;
 }
@@ -107,6 +107,7 @@ char* str_cat(char** dst, const char* src)
 
 int str_printf(char** dst, const char* fmt, ...)
 {
+    return 0;
 }
 
 
